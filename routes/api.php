b3849219a38fb37f7be88c350a9d96e7f379bb25
Peter Ngari <nyashngari@gmail.com>
2026-07-24 18:01:56 +0300

    Fix CI import ordering

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthorizationServerMetadataController;
 use App\Http\Controllers\DeploymentController;
 use App\Http\Controllers\McpController;
-use App\Http\Controllers\AuthorizationServerMetadataController;
 use App\Http\Controllers\OAuthClientRegistrationController;
 use App\Http\Controllers\OAuthMetadataController;
 use App\Http\Controllers\OAuthTokenController;
